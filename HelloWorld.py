56369d87f4253541273c3cd52d7fa8cbfaf7f552
sahilkapila <sahilkapila@gmail.com>
2020-09-12 17:14:04 +0200

    Day1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,9 @@
 print("Hello World")
+age = 20
+sentence = "this is a test"
+
+print("Hello, "+sentence)
+
+print("Just so you know Age: "+str(age))
+
+print("Hello World..\nHello, "+sentence+"\nJust so you know Age: "+str(age))
