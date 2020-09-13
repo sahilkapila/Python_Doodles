2a3b9addec9fc1ffa33fd4d2c433aadbd9d619dd
sahilkapila <sahilkapila@gmail.com>
2020-09-13 17:56:36 +0200

    DAY2

--- Automate_The_Boring_Stuff/Chapter1.py
+++ Automate_The_Boring_Stuff/Chapter1.py
@@ -12,3 +12,13 @@ print(a/b,"\t",names[3]," ",a," ",b)
 print(a*b,"\t",names[4]," ",a," ",b)
 print(a-b,"\t",names[5]," ",a," ",b)
 print(a+b,"\t",names[6]," ",a," ",b)
+
+print("The String replicator\t"*2)
+
+print("Hello world!!!\nWhat is your name? ")
+myname = input()
+print("Ah ACK!! ",myname)
+
+from varname import nameof
+
+print("Length of variable ",nameof(myname)," : ",len(myname))
