0dc97964dd88773d03bd54973d9da86d07d98baf
sahilkapila <sahilkapila@gmail.com>
2020-09-13 23:20:46 +0200

    DAY2

--- Automate_The_Boring_Stuff/Chapter2.py
+++ Automate_The_Boring_Stuff/Chapter2.py
@@ -22,7 +22,7 @@ print(operations[5],"\t",a>=b)
 booleanoperations = ["and","or","not"]
 
 print(booleanoperations)
-
+#ERROR: Does not evaluate the or and not conditions
 for x in booleanoperations:
 #    print(x)
     if (x == "and"):
@@ -33,3 +33,10 @@ for x in booleanoperations:
         (x,"\t",not a," ",not b)
 else:
     print("Finally finished....")
+
+x = 0
+repeat = 5
+print("In a while loop: ",nameof(repeat)," = ",repeat)
+while x < repeat:
+    print("In a while")
+    x = x +1
