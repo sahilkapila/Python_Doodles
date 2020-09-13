5804a18d7228871bf46619422765e783d592a163
sahilkapila <sahilkapila@gmail.com>
2020-09-13 17:34:29 +0200

    DAY2

--- Automate_The_Boring_Stuff/Chapter1.py
+++ Automate_The_Boring_Stuff/Chapter1.py
@@ -3,4 +3,12 @@
 a = 3
 b = 2
 
-print(a**b,"This is a test!!")
+names = ["Exponent", "Modulus", "Integer Division", "Division", "Multiplication","Subtraction","Addition"]
+
+print(a**b,"\t",names[0]," ",a," ",b)
+print(a%b,"\t",names[1]," ",a," ",b)
+print(a//b,"\t",names[2]," ",a," ",b)
+print(a/b,"\t",names[3]," ",a," ",b)
+print(a*b,"\t",names[4]," ",a," ",b)
+print(a-b,"\t",names[5]," ",a," ",b)
+print(a+b,"\t",names[6]," ",a," ",b)
