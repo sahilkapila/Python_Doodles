991897a83343193ebb7d4e3e4b86d2ec6bd6b0ba
sahilkapila <sahilkapila@gmail.com>
2020-09-16 22:02:05 +0200

    DAY4

--- Automate_The_Boring_Stuff/cointoss.py
+++ Automate_The_Boring_Stuff/cointoss.py
@@ -1,11 +1,13 @@
 from varname import nameof
-import random
+import random,sys
 
 def getTOSS(num):
     if num == 1:
         return "Heads"
+        #sys.exit(1)
     elif num == 2:
         return "Tails"
+        #sys.exit(1)
 
 r = random.randint(1,2)
 fortune = getTOSS(r)
