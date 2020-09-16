b1cbc8c118941cdc7749c7e3d662c9fc93732761
sahilkapila <sahilkapila@gmail.com>
2020-09-16 13:15:39 +0200

    DAY4

--- Automate_The_Boring_Stuff/rpsGame.py
+++ Automate_The_Boring_Stuff/rpsGame.py
@@ -16,3 +16,22 @@ wins = 0
 ties = 0
 losses = 0
 ties = 0
+gamelabels = ["Rock","Paper","Scissor"]
+
+optioncontinue = True
+while(optioncontinue):
+    #clear()
+    print(nameof(wins),":\t",wins," ",nameof(ties),":\t",ties," ",nameof(losses),":\t",losses)
+    CPUchoice = random.randint(0,2)
+    print(nameof(CPUchoice),":\t",CPUchoice)
+    print("Enter your choice (0: Rock, 1: Paper, 2: Scissor):")
+    USERchoice = int(input())
+    print(nameof(USERchoice),":\t",USERchoice)
+    if CPUchoice == USERchoice:
+        wins = wins + 1
+    else:
+        losses = losses + 1
+
+    print("Would you like to continue (Type yes), press enter if not : ")
+    optioncontinue = bool(input())
+sys.exit(1)
