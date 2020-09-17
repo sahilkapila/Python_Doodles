f260b423f2a836926210763d0cad254f1485505f
sahilkapila <sahilkapila@gmail.com>
2020-09-17 15:13:50 +0200

    DAY5

--- Automate_The_Boring_Stuff/Chapter3.py
+++ Automate_The_Boring_Stuff/Chapter3.py
@@ -1,6 +1,4 @@
 #Functions
-from varname import nameof
-import sys, random, platform
 
 def hello():
     print("Inside:\tHello()")
@@ -26,3 +24,46 @@ checkvar(testvar)
 testvar = "Hello"
 
 checkvar(testvar)
+
+# Testing the optional parameters with print() .... end and sep
+
+print("this","is","a","test",sep="\t|")
+print("this")
+print("is", end="\t")
+print("a", end="\t")
+print("test")
+
+# Testing the global statement
+
+import random
+def coinTOSS():
+    global side
+    side = random.randint(0,1)
+    return side
+
+side = random.randint(0,1)
+print(side)
+print(coinTOSS())
+print(coinTOSS())
+
+#Try except test
+
+def func1():
+    global num
+    try:
+        print(num,(1/num),sep="\t")
+        num = random.randint(0, 1)
+    except ZeroDivisionError:
+        print("Divide by zero!")
+        num = random.randint(0, 1)
+
+num = random.randint(0,1) #Initiated the global variable
+func1()
+func1()
+func1()
+func1()
+func1()
+func1()
+func1()
+func1()
+func1()
