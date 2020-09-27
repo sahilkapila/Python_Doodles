8d97fd215af8e8d172ff17f9328d568a4d453420
sahilkapila <sahilkapila@gmail.com>
2020-09-27 13:51:52 +0200

    DAY8

--- Automate_The_Boring_Stuff/Chapter5.py
+++ Automate_The_Boring_Stuff/Chapter5.py
@@ -33,3 +33,40 @@ try:
 except KeyError:
     print("Item not found!")
 
+# Now we leverage the keys(), values() and items() methods for dictionaries
+
+dict1 = {
+    "First":"value1",
+    "Second":"value2",
+    "Third":"value3"
+}
+try:
+    print(dict1["third"])
+except KeyError:
+    print("NOTE: the key-value pairs are case sensitive!!")
+
+for keys in dict1.keys():
+    print(nameof(keys),":",keys,sep="\t", end="\n")
+    print(nameof(dict1),"[",keys,"]",":", dict1[keys], sep="\t", end="\n")
+
+for values in dict1.values():
+    print(nameof(values),":", values,sep="\t",end="\n")
+
+# You cannot get an index from a value in dictionary
+
+for keys,values in dict1.items():
+    print(nameof(keys),":",keys, sep="\t")
+    print(nameof(values),":",values, sep="\t")
+
+#check if the value exists in a dictionary
+
+if "value3" in dict1.values():
+    print("Value 3 Found!")
+
+if "Third" not in dict1.keys():
+    print("Value not found!")
+else:
+    print("Third is found!")
+
+#checking if the key exists in the diction
+sys.exit(1)
